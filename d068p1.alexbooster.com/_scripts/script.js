b71736508f675b5ae06ab9283c62d19ee90b4e43
Andy <webdevbooster@gmail.com>
2017-11-18 04:43:39 +0100

    Adding project 1 for day 68

---
+++ d068p1.alexbooster.com/_scripts/script.js
@@ -0,0 +1,5 @@
+    
+function makeLink(user, domainStart, domainEnd) {
+    document.write('<a class="e-l2" href="mailto:' + user + '@' + domainStart + '.' + domainEnd + '">' + user + '@' + domainStart + '.' + domainEnd + '</a>');
+}
+
