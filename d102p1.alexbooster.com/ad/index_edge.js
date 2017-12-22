6a434782e9217152a12466b6dfda481a92996c44
Andy <webdevbooster@gmail.com>
2017-12-22 09:16:16 +0100

    Adding project 1 for day 102

---
+++ d102p1.alexbooster.com/ad/index_edge.js
@@ -0,0 +1,45 @@
+
+(function(compId){var _=null,y=true,n=false,x3='6.0.0',x36='nowrap',e40='${Next}',x4='5.0.0',x6='rgba(0,0,0,0)',x13='700',g='image',e32='${Head}',b='block',e44='${Previous}',x34='droid-sans, serif',x42='Previous',x='text',x41='rgba(255,255,255,1)',c='color',x18='solid',m='rect',x15='0px',e31='${Subhead}',x37='Next',x35='rgba(255,255,255,1.00)',i='none',x21='40',x11='50',p='px',o='opacity',lf='left',x17='rgba(0,0,0,1.00)',x16='56px',e26='${SnowPine}',e27='${Tent3}',x12='Arial, Helvetica, sans-serif',e33='${WinterCover}',l='normal',e30='${Cover2}',x39='auto',bt='bottom',e29='${LeftArrow}',x24='36',d='display',fs='font-size',e28='${SlideTwoHead}',x25='400',x5='6.0.0.400',xc='rgba(0,0,0,1)',x22='droid-serif, serif',x14='break-word';var g9='SnowPine.jpg',g19='Tent.png',g8='Cover.jpg',g2='jquery.easing.1.3.js',g1='jquery-1.7.1.min.js',g7='WinterCover.jpg';var s10="If it is winter in Ontario ...",s38="&gt;",s43="&lt;",s20="It is playtime!",s23="Hike. Camp. Ski. Snowshoe! Explore!";var im='images/',aud='media/',vid='media/',js='js/',fonts={'droid-serif, serif':'<script src=\"http://use.edgefonts.net/droid-serif.js\"></script>','droid-sans, serif':'<script src=\"http://use.edgefonts.net/droid-sans.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[js+g1,js+g2],symbols={"stage":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{id:'WinterCover',v:b,t:g,r:['0px','0px','100%','100%','auto','auto'],o:'1',f:[x6,im+g7,'0%','0%','100%','auto','no-repeat']},{id:'Cover2',v:i,t:g,r:['960','0','100%','100%','auto','auto'],f:[x6,im+g8,'0px','0px']},{id:'SnowPine',v:i,t:g,r:['100%','0%','100%','100%','auto','auto'],f:[x6,im+g9,'0px','0px']},{id:'Head',v:b,t:x,r:['auto','auto','94.1%','15%','3.2%','62%'],text:s10,align:"center",n:[x12,[x11,p],"rgba(255,255,255,1.00)",x13,i,"",x14,""],ts:[x15,x15,x16,x15,""]},{id:'Rectangle',t:m,r:['0px','auto','100%','128px','auto','0px'],o:'0.701416015625',f:[x17],s:[0,xc,x18]},{id:'Tent3',v:i,t:g,r:['-30.4%','auto','292px','146px','auto','-2.2%'],f:[x6,im+g19,'0px','0px']},{id:'RightArrow',symbolName:'Forward',t:m,r:['auto','28.4%','31','75','0%','auto'],cu:'pointer'},{id:'LeftArrow',symbolName:'Back',v:i,t:m,r:['0%','28.4%','31','75','auto','auto'],cu:'pointer'},{id:'Subhead',v:i,t:x,r:['auto','auto','42.5%','12.4%','28.8%','9.6%'],text:s20,align:"center",n:[x22,[x21,p],"rgba(255,255,255,1)",x13,i,l,x14,""]},{id:'SlideTwoHead',v:i,t:x,r:['auto','auto','799','46','8.4%','5.4%'],text:s23,align:"center",n:[x22,[x24,p],"rgba(255,255,255,1)",x25,i,l,x14,""]}],style:{'${Stage}':{isStage:true,r:['null','null','100%','100%','auto','auto'],zr:['555px','1260px','undefined','undefined'],overflow:'hidden',f:[x17]}}},tt:{d:3500,a:y,data:[["eid53",d,2000,0,"easeOutBounce",e26,i,b],["eid47",lf,3000,500,"easeOutBounce",e27,'-30.42%','0.73%'],["eid20",fs,1094,0,"easeOutCubic",e28,'36px','36px'],["eid50",d,1094,0,"easeOutBounce",e29,i,b],["eid37",d,3000,0,"easeOutCubic",e27,i,b],["eid12",d,1046,0,"linear",e30,i,b],["eid52",d,2000,0,"easeOutBounce",e30,b,i],["eid23",d,500,0,"easeOutCubic",e31,i,b],["eid17",d,1046,0,"easeOutCubic",e31,b,i],["eid16",d,1046,0,"linear",e32,b,i],["eid11",lf,1000,1000,"easeOutCubic",e30,'960px','0px'],["eid26",lf,2000,1000,"easeOutCubic",e26,'100%','0%'],["eid54",bt,1094,0,"easeOutCubic",e28,'5.4%','5.4%'],["eid51",d,1046,0,"easeOutBounce",e33,b,i],["eid18",d,1094,0,"easeOutCubic",e28,i,b],["eid14",o,1000,1000,"linear",e33,'1','0']]}},"Forward":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{n:[x34,[60,p],x35,l,i,'','',x36],id:x37,text:s38,t:x,r:[0,0,x39,x39,x39,x39]}],style:{'${symbolSelector}':{r:[_,_,31,75]}}},tt:{d:1000,a:y,l:{"Up":0,"Over":1000},data:[["eid1",c,0,0,"linear",e40,'rgba(255,255,255,1.00)','rgba(255,255,255,1.00)'],["eid2",c,1000,0,"linear",e40,'rgba(255,255,255,1.00)','rgba(185,8,8,1.00)']]}},"Back":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{n:[x34,[60,p],x41,l,i,l,'',x36],t:x,id:x42,text:s43,align:lf,r:[0,0,x39,x39,x39,x39]}],style:{'${symbolSelector}':{r:[_,_,31,75]}}},tt:{d:1000,a:y,l:{"Up":0,"Over":1000},data:[["eid48",c,0,0,"easeOutBounce",e44,'rgba(255,255,255,1)','rgba(255,255,255,1)'],["eid49",c,1000,0,"easeOutBounce",e44,'rgba(255,255,255,1)','rgba(218,11,11,1.00)']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-158684116");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${RightArrow}","mouseover",function(sym,e){sym.getSymbol("RightArrow").play("Over");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${RightArrow}","mouseout",function(sym,e){sym.getSymbol("RightArrow").play("Up");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${RightArrow}","click",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1957,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3500,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${LeftArrow}","mouseout",function(sym,e){sym.getSymbol("LeftArrow").play("Up");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${LeftArrow}","mouseover",function(sym,e){sym.getSymbol("LeftArrow").play("Over");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${LeftArrow}","click",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){$("#Stage").css("margin","auto");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'RightArrow'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1000,function(sym,e){sym.stop();});
+//Edge binding end
+})("Forward");
+//Edge symbol end:'Forward'
+
+//=========================================================
+
+//Edge symbol: 'Back'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+})("Back");
+//Edge symbol end:'Back'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-158684116");
